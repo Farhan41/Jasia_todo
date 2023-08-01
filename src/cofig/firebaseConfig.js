b29288511c4f4939bff3a0ed835c796ed38e2f14
Farhan Ferdous <farhanferdous@gmail.com>
2023-08-01 22:34:42 +0600

    Ayesha

---
+++ src/cofig/firebaseConfig.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDfTAEQhb0TzI_qUacw2kepEa2uTserROI",
+  authDomain: "ayehsa-3d867.firebaseapp.com",
+  projectId: "ayehsa-3d867",
+  storageBucket: "ayehsa-3d867.appspot.com",
+  messagingSenderId: "1053706588469",
+  appId: "1:1053706588469:web:78018fa066f5db9ab23c7c"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default firebaseConfig
